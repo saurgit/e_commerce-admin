1ea4da048ff98f7c98fd64373c394cb3a62a66b2
Saurabh Patel <sp765431@gmail.com>
2024-04-23 23:00:56 +0530

    updated Address

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const base_url = 'http://localhost:5000/api/'
+// const base_url = 'http://localhost:5000/api/'
+export const BASE_URL ="https://e-commerce-api-7yrk.onrender.com/api/"
 let token=""
 if(localStorage.getItem("persist:root")){
     const temp=JSON.parse(localStorage.getItem("persist:root"))
@@ -16,10 +17,10 @@ if(localStorage.getItem("persist:root")){
 // axios.create is a factory to create new  instances of axios with pre-configured options
 
 export const publicRequest = axios.create({
-    baseURL:base_url,
+    baseURL:BASE_URL,
 })
 
 export const userRequest = axios.create({
-    baseURL:base_url,
+    baseURL:BASE_URL,
     headers:{token:`Bearer ${token}`}
 })
